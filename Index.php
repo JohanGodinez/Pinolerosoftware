736c0af77bbf747202c5fa9af7f28137032c247d
JohanGodinez <johang77@hotmail.com>
2016-04-11 10:40:38 -0600

    eliminando archivo de mas

--- Index.php
+++ Index.php
@@ -1,9 +1,20 @@
 <html>
   <head>
     <title>Pinolero</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/stylo.css">
   </head>
   <body>
-    <h1>Parece que si funciona</h1>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Info</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    
+    
+    <script type="text/javascript" src="js/jquery-2.2.2.min.js" />
+    <script type="text/javascript" src="js/bootstrap.min.js" />
   </body>
 </html>
